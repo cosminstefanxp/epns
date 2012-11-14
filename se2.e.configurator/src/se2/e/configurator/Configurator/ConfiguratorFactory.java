c3171b5305a180be87f07bd6808ae4f2ae360740
Ruxandra Nistor <roxanne0389@gmail.com>
2012-11-14 11:16:06 +0000

    Updated new Configurator project...

---
+++ se2.e.configurator/src/se2/e/configurator/Configurator/ConfiguratorFactory.java
@@ -0,0 +1,42 @@
+/**
+ */
+package se2.e.configurator.Configurator;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see se2.e.configurator.Configurator.ConfiguratorPackage
+ * @generated
+ */
+public interface ConfiguratorFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	ConfiguratorFactory eINSTANCE = se2.e.configurator.Configurator.impl.ConfiguratorFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Configurator</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Configurator</em>'.
+	 * @generated
+	 */
+	Configurator createConfigurator();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	ConfiguratorPackage getConfiguratorPackage();
+
+} //ConfiguratorFactory

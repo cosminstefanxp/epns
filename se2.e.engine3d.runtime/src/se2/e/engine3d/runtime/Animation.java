c5a304a72ac9638459e6f362fdd311ebfcfd7397
s112674 <s112674@eac5aa46-858a-4102-944a-bd505b0b48df>
2012-11-14 12:42:37 +0000

    Recommitting :-)

---
+++ se2.e.engine3d.runtime/src/se2/e/engine3d/runtime/Animation.java
@@ -0,0 +1,31 @@
+package se2.e.engine3d.runtime;
+
+public interface Animation {
+
+	void setParent(Animation parent);
+	
+	/**
+	 * The target animated object of this animation.
+	 * 
+	 * @return the target.
+	 */
+	AnimatedObject getTarget();
+	
+	/**
+	 * Get tick from when this animation was cretated.
+	 * 
+	 * @return the start tick.
+	 */
+	long getStartTick();
+	
+	/**
+	 * Recalculate the animated objects data.
+	 * 
+	 * Returns true if the animation is still running,
+	 * otherwise false.
+	 * 
+	 * @return whether the animation is still running.
+	 */
+	public boolean recalculate();
+	
+	}

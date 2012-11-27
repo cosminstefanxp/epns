af636d7d9b65daed8460f3fb76676b7cf548276f
s114414 <s114414@eac5aa46-858a-4102-944a-bd505b0b48df>
2012-11-27 14:29:51 +0000

    Fixed test...

--- se2.e.utilities/src/se2/e/utilities/test/Vector2DTest.java
+++ se2.e.utilities/src/se2/e/utilities/test/Vector2DTest.java
@@ -1,20 +1,20 @@
 package se2.e.utilities.test;
 
-import static org.junit.Assert.*;
-import static java.lang.Math.*;
+import static java.lang.Math.PI;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import se2.e.utilities.Vector;
+import se2.e.utilities.Vector2D;
 
 
-public class VectorTest {
+public class Vector2DTest {
 
 	@Test
 	public void testCreateCartesian() {
 		double x = 3.4;
 		double y = -2.0;
-		Vector v = Vector.cartesian(x, y);
+		Vector2D v = Vector2D.cartesian(x, y);
 		assertEquals(x, v.getX(), 1e-5);
 		assertEquals(y, v.getY(), 1e-5);
 		}
@@ -23,7 +23,7 @@ public class VectorTest {
 	public void testCreatePolar30() {
 		double angle = 30.0*PI/180.0;
 		double length = 10;
-		Vector v = Vector.polar(angle, length);
+		Vector2D v = Vector2D.polar(angle, length);
 		assertEquals(5.0, v.getY(), 1e-5);
 		}
 
@@ -31,7 +31,7 @@ public class VectorTest {
 	public void testCreatePolar60() {
 		double angle = 60.0*PI/180.0;
 		double length = 10;
-		Vector v = Vector.polar(angle, length);
+		Vector2D v = Vector2D.polar(angle, length);
 		assertEquals(5.0, v.getX(), 1e-5);
 		}
 	
@@ -39,7 +39,7 @@ public class VectorTest {
 	public void testUnity() {
 		double x = 10.0;
 		double y = 20.0;
-		Vector v = Vector.cartesian(x, y).unity();
+		Vector2D v = Vector2D.cartesian(x, y).unity();
 		assertEquals(1.0, v.getLength(), 1e-5);
 		}
 	
@@ -47,7 +47,7 @@ public class VectorTest {
 	public void testLength() {
 		double x = 10.0;
 		double y = 20.0;
-		Vector v = Vector.cartesian(x, y);
+		Vector2D v = Vector2D.cartesian(x, y);
 		double angle = v.getAngle();
 		v.setLength(10.0);
 		assertEquals(angle, v.getAngle(), 1e-5);

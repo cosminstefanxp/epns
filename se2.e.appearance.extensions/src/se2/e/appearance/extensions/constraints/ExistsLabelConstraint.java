494e3755f91d990fc1851e9b7a4b056356af4b60
s121874 <s121874@eac5aa46-858a-4102-944a-bd505b0b48df>
2012-11-28 10:53:32 +0000

    Appearance constraints

---
+++ se2.e.appearance.extensions/src/se2/e/appearance/extensions/constraints/ExistsLabelConstraint.java
@@ -0,0 +1,23 @@
+package se2.e.appearance.extensions.constraints;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+
+import appearance.AppearanceInfo;
+
+public class ExistsLabelConstraint extends AbstractModelConstraint {
+
+	@Override
+	public IStatus validate(IValidationContext vc) {
+		EObject object = vc.getTarget();
+		if (object instanceof AppearanceInfo) {
+			AppearanceInfo appInfo = (AppearanceInfo) object;
+			if(appInfo.getLabel() == null || appInfo.getLabel().equals(""))
+				return vc.createFailureStatus(new Object[] {appInfo});
+		}		
+		return vc.createSuccessStatus();
+	}
+	
+}
